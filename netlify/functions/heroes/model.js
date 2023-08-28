180f994da6b129ed5b152ad9fd7c81b9c4363600
MarizzaMil <marizza.mil89@gmail.com>
2023-08-28 14:36:24 +0300

    crud

--- netlify/functions/heroes/model.js
+++ netlify/functions/heroes/model.js
@@ -5,15 +5,52 @@ const getHeroesModel = () => {
     findAll: async (callback) => {
       try {
         const snapshot = await firestore.collection('heroes').get();
-        console.log(snapshot)
-        const heroes = snapshot.docs.map((doc) => doc.data());
+        const heroes = snapshot.docs.map((doc) => {
+          const data = doc.data();
+          data.id = doc.id; // Add the id field to the data
+          return data;
+        });
         callback(heroes);
       } catch (err) {
         console.error('Firestore query error:', err.message);
         callback([]);
       }
     },
-    // ... other methods
+    create: async (name, superPower, callback) => {
+      try {
+        await firestore.collection('heroes').add({
+          name,
+          superPower,
+        });
+        callback(null, 'Hero created successfully');
+      } catch (err) {
+        console.error('Firestore query error:', err.message);
+        callback('Error creating hero');
+      }
+    },
+
+    update: async (id, name, superPower, callback) => {
+      try {
+        await firestore.collection('heroes').doc(id).update({
+          name,
+          superPower,
+        });
+        callback(null, 'Hero updated successfully');
+      } catch (err) {
+        console.error('Firestore query error:', err.message);
+        callback('Error updating hero');
+      }
+    },
+
+    delete: async (id, callback) => {
+      try {
+        await firestore.collection('heroes').doc(id).delete();
+        callback(null, 'Hero deleted successfully');
+      } catch (err) {
+        console.error('Firestore query error:', err.message);
+        callback('Error deleting hero');
+      }
+    },
   };
 };
 
